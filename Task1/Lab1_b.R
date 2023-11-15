8d7d76a5e632d0e4d6c44c15f04ed7062e0b5abd
edvmar <edvmar@student.chalmers.se>
2023-11-15 15:47:53 +0100

    Added complex Gaussian PDF and plotting. Need to review if it's correct.

--- Task1/Lab1_b.R
+++ Task1/Lab1_b.R
@@ -11,12 +11,12 @@ library(cmvnorm)
 #   Gaussian detector
 #   Compound Gaussian clutter
 
-n = 1000 # Number of simulations in Monte Carlo
+n = 1000  # Number of simulations in Monte Carlo / sample size 
 alpha = 2
 theta = 0
 s <- complex(modulus = alpha, argument = theta) # signal 
 
-etas = c(0.1, 0.5, 1, 2, 5, 10) # change later
+etas = c(0.5, 1, 2, 5, 10) # change later
 P_FA = rep(0, length(etas)) # store for plotting 
 P_TD = rep(0, length(etas))
 
@@ -32,45 +32,41 @@ for (iEta in 1:length(etas)){
   r_0 = c
   r_1 = c + s
   
-  # TODO: Find these correctly from PDF for CN 
-  # TODO: Move to for loop? Don't need to store them.. 
-  f0_r0 = rep(0,n) # f(r|H_0) for r in r_0, i.e. PDF for CN(0,1)
-  f1_r0 = rep(0,n) # f(r|H_1) for r in r_0, i.e. PDF for CN(s,1)
   
-  f0_r1 = rep(0,n) # f(r|H_0) for r in r_1, i.e. PDF for CN(0,1)
-  f1_r1 = rep(0,n) # f(r|H_1) for r in r_1, i.e. PDF for CN(s,1)
-  
-  #for(i in 1:n){
-  #  f_0[i] <- dcmvnorm(r_0[i], 0, 1)
-  #  f_0[i] <- dcmvnorm(r_1[i], 0, 1)
-  #}
-  
+  # TODO: Find these correctly from PDF for CN => I think I did it right now?
+  # f( 2+i | CN(0,1) ] = dcmvnorm(c(2,1), c(0,0), diag(2)/2)
+ 
   for (j in 1:n){
-    
     #False Alarm 
-    if(f1_r0[j]/f0_r0[j] > eta){ # False detection 
-      P_FA[iEta] = P_FA[iEta] + f0_r0[j]
+    f0_r0 <- dcmvnorm(c(Re(r_0[j]), Im(r_0[j])), c(0,0), diag(2)/2)             # f(r|H0), r \in r_0 
+    f1_r0 <- dcmvnorm(c(Re(r_0[j]), Im(r_0[j])), c(Re(s),Im(s)), diag(2)/2)     # f(r|H1), r \in r_0 
+
+    if(f1_r0/f0_r0 > eta){ # False detection 
+      P_FA[iEta] = P_FA[iEta] + f0_r0
     }
+    
     #True Detection
-    if(f1_r1[j]/f0_r1[j] > eta){ # True detection 
-      P_TD[iEta] = P_TD[iEta] + f1_r1[j]
+    f0_r1 <- dcmvnorm(c(Re(r_1[j]), Im(r_1[j])), c(0,0), diag(2)/2)             # f(r|H0), r \in r_1
+    f1_r1 <- dcmvnorm(c(Re(r_1[j]), Im(r_1[j])), c(Re(s),Im(s)), diag(2)/2)     # f(r|H1), r \in r_1 
+    
+    if(f1_r1/f0_r1 > eta){ # True detection 
+      P_TD[iEta] = P_TD[iEta] + f1_r1
     }
   }
 
 }
 
-P_FA = P_FA/N.  #Normalize 
-P_TD = P_TD/N
+P_FA = P_FA/n  #Normalize 
+P_TD = P_TD/n
 
 
 # TODO: Plot P_FA vs P_TD correctly with log scales etc
 # TODO: Guess we should do this for several SIR:s as well in the same plot.. 
-P_FA = c(0, 0.2, 0.3, 0.4)
-P_TD = c(0, 0.4, 0.6, 0.8)
 plot(P_FA, P_TD, type = "b", frame = FALSE, pch = 19, 
      col = "black", xlab = "P_FA", ylab = "P_TD")
 legend("right", legend=c("SIR = ? "),
        col=c("black"), lty = 1:2, cex=0.8)
+
 
 
 
