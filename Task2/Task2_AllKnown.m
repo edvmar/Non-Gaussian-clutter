05a22b6e6360d618919a5e3cf6d79b28b483d2e8
samwin123 <samwin@student.chalmers.se>
2023-12-06 16:44:30 +0100

    Revert "Continuation of changes"
    This reverts commit eaec5349aeb64dba67c3a2ed36650c6b34468800.

--- Task2/Task2_AllKnown.m
+++ Task2/Task2_AllKnown.m
@@ -7,12 +7,10 @@
 
 clc, clear, close all
 
-
-
-
 sampleSize = 1e4;
 sigma = 1;
 rMax  = 10*sigma; % R values calculating for the inverse
+
 
 numberOfPulses    = 10; % 128
 numberOfDistances = 8;  % 100
@@ -34,55 +32,53 @@ L = chol(toeplitzMatrix + epsilon*eye(numberOfPulses));
 toeplitzMatrixInverse = inv(toeplitzMatrix);
 
 
-numberOfEtaValues = 10;
+numberOfEtaValues = 5000;
 etaValues = linspace(0.001, 100, numberOfEtaValues);
 
 sumFA = zeros(1, numberOfEtaValues); % Add for other clutters
 sumTD = zeros(1, numberOfEtaValues);
 
 
-% Complex Compound Gaussian
-F = @(x) 1 - TailDistributionComplexGaussian(abs(x).^2, 0, sigma);  % eqn (12) 
-h_n = @(x) TailDistributionComplexGaussian(x, numberOfPulses, sigma);
 
-% Complex K distribution
-% Add later depending on what they say
+LR_FA = zeros(1, sampleSize);
+LR_TD = zeros(1, sampleSize);
 
+% Sampling.. gör snabbare senare 
+for i = 1:sampleSize
+    
+    F = @(x) 1 - TailDistributionComplexGaussian(abs(x).^2, 0, sigma);  % eqn (12) 
+    CUTnoSignal = Sampling(numberOfPulses, rMax, L, F);
+    CUTsignal = CUTnoSignal + signal;
+    
+    % pFA
+    q0_H0 = real(CUTnoSignal*toeplitzMatrixInverse*CUTnoSignal');
+    q1_H0 = real((CUTnoSignal-signal)*toeplitzMatrixInverse*(CUTnoSignal-signal)');
+    LR_FA(i) = TailDistributionComplexGaussian(q1_H0, numberOfPulses, sigma)/...
+                   TailDistributionComplexGaussian(q0_H0, numberOfPulses, sigma);
+    
+    % pTD
+    q0_H1 = real(CUTsignal*toeplitzMatrixInverse*CUTsignal');
+    q1_H1 = real((CUTsignal-signal)*toeplitzMatrixInverse*(CUTsignal-signal)');
+    LR_TD(i) = TailDistributionComplexGaussian(q1_H1, numberOfPulses, sigma)/...
+                    TailDistributionComplexGaussian(q0_H1, numberOfPulses, sigma);
+
+end
 
 
 for iEta = 1:numberOfEtaValues
-    % Sampling.. gör snabbare senare 
-    LR_FA = zeros(1, sampleSize);
-    LR_TD = zeros(1, sampleSize);
-    eta = etaValues(iEta);
-    
-    for i = 1:sampleSize
-        
-        CUTnoSignal = Sampling(numberOfPulses, rMax, L, F);
-        CUTsignal = CUTnoSignal + signal;
-        
-        % pFA Stämmer dessa verkligen? Tycker vi ska titta på en separat rad. 
-        q0_H0 = real(CUTnoSignal*toeplitzMatrixInverse*CUTnoSignal');
-        q1_H0 = real((CUTnoSignal-signal)*toeplitzMatrixInverse*(CUTnoSignal-signal)');
-        LR_FA(i) = h_n(q1_H0)/...
-                       h_n(q0_H0);
-        
-        % pTD
-        q0_H1 = real(CUTsignal*toeplitzMatrixInverse*CUTsignal');
-        q1_H1 = real((CUTsignal-signal)*toeplitzMatrixInverse*(CUTsignal-signal)');
-        LR_TD(i) = h_n(q1_H1)/...
-                        h_n(q0_H1);
-    end
 
-    sumFA(1, iEta) = sum((LR_FA>eta));
-    sumTD(1, iEta) = sum((LR_TD>eta));
+        eta = etaValues(iEta);
+
+        sumFA(1, iEta) = sum((LR_FA>eta));
+        sumTD(1, iEta) = sum((LR_TD>eta));
+
 end
 
 pFalseAlarm = sumFA/sampleSize;
 pDetection = sumTD/sampleSize;
 
+%%
 hold on
-
 %for iSIR = 1:length(SIRs)
 %    plot(pFalseAlarm(iSIR,:), pDetection(iSIR, :), LineWidth=1.5)
 %end
@@ -98,11 +94,7 @@ xlabel('P_{FA}'), ylabel('P_{TD}')
 
 
 
-%%
-q0_H0
-q0_H1
-q1_H0
-q1_H1
+
 
 
 
