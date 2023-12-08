c301d06fd56f130ccbea94be7c37db8b03e075f2
samwin123 <samwin@student.chalmers.se>
2023-12-08 10:49:38 +0100

    Hej
    Hej

--- Task2/Sampling.m
+++ Task2/Sampling.m
@@ -6,28 +6,30 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function rangeBin = Sampling(numberOfPulses, rMax, sigma, L, F)
+function rangeBin = Sampling(numberOfPulses, sampleSize, rMax, sigma, L, F)
     
     
     % Table for cdf x,y values
-    domain = linspace(0, rMax, rMax*1000);
-    range = F(domain);
+    domain = linspace(0, rMax, rMax*10);
+    range = F(domain)';
+    range_rep = repmat(range,1,sampleSize)';
 
-    uniformSample = rand(1, numberOfPulses);
-    xSample = zeros(numberOfPulses, 1);
+    uniformSample = rand(numberOfPulses, sampleSize);
+    xSample = zeros(numberOfPulses, sampleSize);
     
+
     % Sample x
     for j = 1:numberOfPulses
-        uniform = uniformSample(j);
-        [~, index] = min(abs(range-uniform));
+        uniform = uniformSample(j,:)';
+        [~, index] = min(abs((range_rep-uniform)'));
         radius = domain(index);
         
-        thetas = rand(1)*2*pi;
-        xSample(j) = exp(thetas*1i)*radius; 
+        thetas = rand(1,sampleSize)*2*pi;
+        xSample(j,:) = exp(thetas*1i).*radius; 
     end
 
     % d_k = L x
-    rangeBin = (L*xSample)';
+    rangeBin = (L*xSample);
 
 end
 
